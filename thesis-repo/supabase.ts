d02149c933c0744e5dba62578298e75b79e77e85
Renard B. Macorol <rmacorol18@gmail.com>
2025-02-14 16:36:55 +0800

    config supabase

--- thesis-repo/supabase.ts
+++ thesis-repo/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl ="https:/kiupsvuzemscpmbqqyrl.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpdXBzdnV6ZW1zY3BtYnFxeXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NzA3OTEsImV4cCI6MjA1NTA0Njc5MX0.sM9QYouYKu4EKHAY7XENKp3acs0OPf8V7NJ7ktQzFP8"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if(!supabaseUrl || !supabaseAnonKey){
     throw new Error("Missing supabase environment variable");
 }
