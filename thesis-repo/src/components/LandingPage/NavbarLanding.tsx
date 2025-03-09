b1bb52f6e9ea0c14d72509b353e23b83ec141153
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-03-10 05:26:53 +0800

    Update NavbarLanding.tsx

--- thesis-repo/src/components/LandingPage/NavbarLanding.tsx
+++ thesis-repo/src/components/LandingPage/NavbarLanding.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { useNavigate } from "react-router-dom";
-import CicsLogo from "../global/CicsLogo";
-import SwitchTheme from "../global/SwitchTheme";
-
-const NavBarLanding = () => {
-    const navigate = useNavigate();
-    const handleSignIn = async () => {
-        navigate('/SignInPage')
-    }
-    return(
-        <nav className="bg-white flex items-center justify-between p-4">
-            <div className="flex gap-5">
-            <CicsLogo/>
-            <span className="text-4xl font-semibold">TheCICS</span>
-            </div>
-            <div className="flex gap-8">
-            <button className="p-3 bg-blue-400 text-2xl rounded-3xl"onClick={handleSignIn}>Sign-In</button>
-            <SwitchTheme/>
-            </div>
-        </nav>
-    )
-}
-
-export default NavBarLanding;
-=======
 import { useNavigate } from 'react-router-dom';
 import CicsLogo from '../global/CicsLogo';
 import SwitchTheme from '../global/SwitchTheme';
@@ -56,4 +30,3 @@ const NavBarLanding = () => {
 export default NavBarLanding;
 
 //🍓
->>>>>>> origin/main
