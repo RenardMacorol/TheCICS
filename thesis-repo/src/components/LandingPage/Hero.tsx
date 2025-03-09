a2fe0fe03f11c4560cb74c9cfad8f77477dc347b
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-03-10 05:26:40 +0800

    Update Hero.tsx

--- thesis-repo/src/components/LandingPage/Hero.tsx
+++ thesis-repo/src/components/LandingPage/Hero.tsx
@@ -1,8 +1,5 @@
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import Button from "../global/Button";
->>>>>>> origin/main
 
 const Hero = () => {
     const navigate = useNavigate();
@@ -10,22 +7,6 @@ const Hero = () => {
         navigate('/SignInPage')
     }
     return(
-<<<<<<< HEAD
-        <div className="relative w-full flex-1 flex flex-col items-center justify-center pb-20">
-                {/*place holder*/}
-                <img className="w-full h-200 object-cover"
-                src="animeLanding.jpg"/>
-            <button onClick={handleSignIn} className="mt-6 px-6 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
-                Get Started
-            </button>
-            
-            
-        </div>
-    )
-}
-
-export default Hero;
-=======
         <div className="relative w-full flex-1 flex items-center justify-center pb-20">
         <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-6xl px-4">
             {/* Mascot Image */}
@@ -62,4 +43,3 @@ export default Hero;
 export default Hero;
 
 // 🍓
->>>>>>> origin/main
