0859560729d2581e89460aa7f2d3860742e8c146
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-03-10 05:29:07 +0800

    Update SignInPage.tsx

--- thesis-repo/src/pages/SignInPage.tsx
+++ thesis-repo/src/pages/SignInPage.tsx
@@ -3,40 +3,13 @@ import SignInButton from "../components/SignInButton";
 const SignInPage = () => {
     return(
         <div className="flex h-screen bg-gray-900 text-gray-100">
-<<<<<<< HEAD
-            {/*left SideBar */}
-            <div className="w-1/2 bg-gray-300 relative">
-                {/*Bg image */}
-                <img className="w-full h-full object-cover"
-                src="/AnimeLogin.jpg"/>
-                <div className="absolute top-6 left-6 flex items-center
-                gap-3 bg-gray-800 bg-opacity-60 p-3 rounded-lg">
-                    <div className="w-1/2 bg-gray-300 flex flex-row ">
-                    <img src="none" alt="Logo" className="w-10 h-10 rounded-full"/>
-                    <div className="w-20 h-20 bg-gray-400 rounded-full flex ">
-                    {/*This Place Holder for Icon */}
-                    <span>X</span>
- 
-                </div>
-           </div>
-            <p className="mt-6 text-2xl font-semibold text-white-700">TheCICS</p>
-            </div>
- 
-            </div>
-           
-            {/*Right SideBar */}
-            <div className="w-1/2 bg-gray-300 text-gray-900 flex flex-col justify-center items-center">
-=======
             {/*left SideBar */} 
             <div className="w-1/2 bg-purple-700 relative flex items-center justify-center">
                 {/* Mascot Image */}
->>>>>>> origin/main
                 <div className="text-center ">
                 <p className="text-2xl font-semibold">Log-in Using Institutional Email</p>
                 <SignInButton/>
                 </div>
-<<<<<<< HEAD
-=======
 
             {/* Logo */}
             <div className="absolute top-6 left-6 bg-gray-800 bg-opacity-60 p-3 rounded-lg">
@@ -55,16 +28,11 @@ const SignInPage = () => {
                     src="/Chumchum.png"
                     alt="Chumchum Mascot" 
                     />
->>>>>>> origin/main
             </div>
         </div>
     )
 }
 
-<<<<<<< HEAD
-export default SignInPage;
-=======
 export default SignInPage;
 
 // 🚧 WORKING!!!
->>>>>>> origin/main
