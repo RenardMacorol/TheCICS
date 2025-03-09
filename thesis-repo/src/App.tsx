45c51355194df12a1b00312331fd702cffa920e4
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-03-10 05:41:15 +0800

    Update App.tsx

--- thesis-repo/src/App.tsx
+++ thesis-repo/src/App.tsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/DashboardThesis';
 import LandingPage from './pages/LandingPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 
 const App = () => {
@@ -43,6 +44,7 @@ const { data: authListener } = supabase.auth.onAuthStateChange((_, session) => {
         <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
         <Route path='/SignInPage' element={<SignInPage/>}/>
         <Route path='/SignUpPage' element={<SignUpPage/>}/>
+        <Route path='/admin' element={<AdminDashboard/>}/>
       </Routes>
     </Router>
   );
