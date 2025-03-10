3bd4115f8c8dece338f66358e9b0f136ce3d164e
Renard B. Macorol <rmacorol18@gmail.com>
2025-03-10 08:43:50 +0800

    Fix the merge Issue

--- thesis-repo/src/components/SignInButton.tsx
+++ thesis-repo/src/components/SignInButton.tsx
@@ -3,10 +3,6 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Login from '../service/Login';
 import { User } from '@supabase/supabase-js';
-<<<<<<< HEAD
-=======
-import Button from './global/Button';
->>>>>>> origin/main
 
 const SignIn = () => {
     const navigate = useNavigate();
@@ -30,23 +26,10 @@ const SignIn = () => {
 
   return (
     <div className='m-10 flex items-center justify-center  bg-white-100'>
-<<<<<<< HEAD
       <button className="p-5 flex items-center rounded-lg  bg-blue-400 hover:bg-blue-500"onClick={Login}>
         <img className="w-5 h-5" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"></img>
         <span >Sign in with Google </span>
         </button>
-=======
-      <Button 
-            variant="whitePurple" 
-            size="medium"  
-            onClick={Login}> 
-            Sign in with Google
-        </Button>
-      {/* <button className="p-5 flex items-center rounded-lg  bg-blue-400 hover:bg-blue-500"onClick={Login}>
-        <img className="w-5 h-5" src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"></img>
-        <span >Sign in with Google </span>
-        </button> */}
->>>>>>> origin/main
     </div>
   );
 
