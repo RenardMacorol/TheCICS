53e0d59adbb6c6da50ff507681d23210e8263b07
Renard B. Macorol <rmacorol18@gmail.com>
2025-03-15 14:45:24 +0800

    Fix User Creation issue

--- thesis-repo/src/pages/DashboardThesis.tsx
+++ thesis-repo/src/pages/DashboardThesis.tsx
@@ -3,33 +3,48 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from '../api/supabase';
 import { User } from "@supabase/supabase-js";
 import ContentList from "../components/DashThesis/ContentList";
-import DashNavTop from "../components/DashThesis/DashNavTop";
+import DashNavTop from '../components/DashThesis/DashNavTop';
 import FilterButton from "../components/DashThesis/FilterButton";
 const Dashboard = () => {
   const navigate = useNavigate();
   const [, setUser] = useState<User |null>(null);
 
   useEffect(() => {
-    const fetchUser = async () =>{
-      const {data:{user}} = await supabase.auth.getUser();
-      if(user){
-        setUser(user);
-      } else {
-        navigate("/")
+    const fetchUser = async () => {
+      const { data: { user }, error } = await supabase.auth.getUser();
+
+      if (error||!user || !user.email?.endsWith("@neu.edu.ph")) {
+        navigate("/"); // ✅ Redirect to login if not logged in
+      }else{
+      const fullname = user.user_metadata?.full_name || "User";
+      const {error } = await supabase.from("Users").insert({
+          userID: user.id,
+          name: fullname,
+          email: user.email,
+          role: "Student",
+          googleAuthID: user.id,
+          profilePicture: null,
+          dateRegistered: new Date().toISOString()
+        })
+        if(error) console.error("Error here", error)
+        console.log("The User Creation was success")
+
+      setUser(user);
+ 
       }
-    }
-    fetchUser()
-  }, [navigate])
-  
+      
+   };
 
-  return (
-    <div className="bg-gray-100 min-h-screen text-gray-900">
-      <DashNavTop/>
-      <FilterButton/>
-      <ContentList/>
-    </div>
-);
+    fetchUser();
+  }, [navigate]);
 
+return(
+  <div className="bg-gray-100 min-h-screen text-gray-900">
+    <DashNavTop/>
+    <FilterButton/>
+    <ContentList/>
+  </div>
+)
 }
 
 export default Dashboard;
