3892077983d7d740630666342249838a46b93439
Juliana R. Mancera <mancerajuliana98@gmail.com>
2025-04-19 22:26:39 +0800

    temporary remove restriction

--- thesis-repo/src/pages/dashboard/DashboardThesis.tsx
+++ thesis-repo/src/pages/dashboard/DashboardThesis.tsx
@@ -4,7 +4,7 @@ import { User } from "@supabase/supabase-js";
 import ContentList from "../../components/dashboard/ContentList";
 import DashNavTop from '../../components/dashboard/DashNavTop';
 import FilterButton from "../../components/dashboard/FilterButton";
-import restrictChecker from "../../service/UserHandler/RestrictChecker";
+//import restrictChecker from "../../service/UserHandler/RestrictChecker";
 import ValidUser from '../../service/UserHandler/ValidUser';
 import NewUser from "../../service/UserHandler/NewUser";
 import InsertNewUser from "../../service/UserHandler/InsertNewUser";
@@ -18,7 +18,7 @@ interface FilterState {
 const Dashboard = () => {
   const [userScannned, setUserScanned] = useState(false);
   const [, setUser] = useState<User |null>(null);
-  const [restrict, setRestrict] = useState(false);
+ // const [restrict, setRestrict] = useState(false);
 
   const [searchQuery, setSearchQuery] = useState("");
   const [filters, setFilters] = useState<FilterState>({
@@ -37,7 +37,7 @@ const Dashboard = () => {
           const newUser = new InsertNewUser();
           newUser.insertNewUser(user)
         }
-      setRestrict(await restrictChecker(user.id))
+      //setRestrict(await restrictChecker(user.id))
       setUser(user);
       }
     }
@@ -56,7 +56,7 @@ const Dashboard = () => {
   return (
     <div className="bg-gray-100 min-h-screen text-white">
     <DashNavTop setSearchQuery={setSearchQuery} searchQuery={""}/>
-    {restrict ? 
+    {/*{restrict ? 
     <p className="flex justify-center items-center font-bold text-5xl text-blue-500">You Are Restricted to this page Please Contact Support -Wonka</p> 
     :
     (
@@ -65,7 +65,11 @@ const Dashboard = () => {
     <ContentList searchQuery={searchQuery} filters={filters}/>
       </div>
     )
-    }
+    }*/}
+    <div>
+    <FilterButton onFilterChange={handleFilterChange}/>
+    <ContentList searchQuery={searchQuery} filters={filters}/>
+      </div>
   </div>
   );
 };
